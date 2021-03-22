8ed7653506e60ef63fbb6ee18a936d4bccc8f4c9
Bitter <vladkastom97@mail.ru>
2021-03-22 16:43:57 +0300

    vidodo-strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8aa42f7c4592db0e67076389482b8c50'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d76a367-d04c-45f4-a791-5825271d5bbf'
+};
